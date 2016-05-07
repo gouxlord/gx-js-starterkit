de5974c062daad575479d67165f2807135dec531
pierregoux <goux.pierre@gmail.com>
2016-05-07 11:03:04 +0200

    Add css loader

--- module-bundler/webpack.base.loaders.js
+++ module-bundler/webpack.base.loaders.js
@@ -8,5 +8,6 @@ module.exports = [
             plugins: ['transform-runtime'],
             presets: ['es2015', 'react', 'stage-0']
         }
-    }
+    },
+    { test: /\.css$/, loader: "style-loader!css-loader?modules" },
 ]
